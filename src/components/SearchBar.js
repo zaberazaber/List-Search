2938062b1fe6d9b9519f8f4e31932b515d1f401d
Mohammad Zubair <zuby@Mohammads-MacBook-Air.local>
2021-09-18 02:45:52 +0530

    added a callback for child to parent communication

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,10 +3,10 @@ import React from 'react';
 class SearchBar extends React.Component {
   state = { term: '' };
 
-  onFormSubmit(event) {
-    event.preventDefault();
-
-    console.log(this.state.term);
+  onFormSubmit = (event) => {
+    event.preventDefault(); //we do this to prevent the default behaviour on form submition on enter
+    this.props.onSubmitCallback(this.state.term) // calling the call back
+    // console.log(this.state.term); // this will also throw an error cannot read state of undefined. to resolve this error we have to bind the function or we have to make this function an arrow function
   }
 
   render() {
